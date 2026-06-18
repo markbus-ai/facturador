f0d03510c2c4fa95ccce9fd8768a4ea6ca4c1439
Marcos Bustos <marcosbustos.dev@gmail.com>
2026-06-18 09:32:24 -0300

    initial commit

--- src/database/UserRepository.h
+++ src/database/UserRepository.h
@@ -1,30 +1,30 @@
 #pragma once
-#include <QString>
 #include "core/Result.h"
+#include <QString>
 
 class QSqlQuery;
 
 struct UserData {
-    int id = 0;
-    QString username;
-    QString role;
+  int id = 0;
+  QString username;
+  QString role;
 };
 
 class UserRepository {
 public:
-    static UserRepository &instance();
+  static UserRepository &instance();
 
-    Result registerUser(const QString &username, const QString &password,
-                        const QString &role = "user");
-    ResultOr<UserData> validateUser(const QString &username,
-                                    const QString &password);
-    ResultOr<UserData> findById(int id);
-    ResultOr<UserData> findByUsername(const QString &username);
-    QList<UserData> all();
-    Result changeRole(int userId, const QString &newRole);
-    Result remove(int userId);
+  Result registerUser(const QString &username, const QString &password,
+                      const QString &role = "user");
+  ResultOr<UserData> validateUser(const QString &username,
+                                  const QString &password);
+  ResultOr<UserData> findById(int id);
+  ResultOr<UserData> findByUsername(const QString &username);
+  QList<UserData> all();
+  Result changeRole(int userId, const QString &newRole);
+  Result remove(int userId);
 
 private:
-    UserRepository() = default;
-    UserData rowToUser(const QSqlQuery &q) const;
+  UserRepository() = default;
+  UserData rowToUser(const QSqlQuery &q) const;
 };
